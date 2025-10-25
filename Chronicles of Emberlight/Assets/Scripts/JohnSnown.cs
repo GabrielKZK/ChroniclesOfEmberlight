b86efd20315c754db8e77eeb43215dc12a3fcad9
GabrielKZK <gabriel.nasimeto34@gmail.com>
2025-10-25 05:25:43 -0300

    feat: novas animações e scripts (Unity: Assets/ + ProjectSettings/ + Packages/)

--- Chronicles of Emberlight/Assets/Scripts/JohnSnown.cs
+++ Chronicles of Emberlight/Assets/Scripts/JohnSnown.cs
@@ -4,5 +4,87 @@ using UnityEngine;
 
 public class JohnSnown : MonoBehaviour
 {
-    
+    [SerializeField] private float speed;
+    [SerializeField] private float runSpeed;
+
+    private Rigidbody2D rig;
+
+    private float initialSpeed;
+    private bool _isRunning;
+    private bool _isRolling;
+    private Vector2 _direction;
+
+    public Vector2 direction
+    {
+        get { return _direction; }
+        set { _direction = value; }
+    }
+    public bool isRolling
+    {
+        get { return _isRolling; }
+        set { _isRolling = value; }
+    }
+    public bool isRunning
+    {
+        get { return _isRunning; }
+        set { _isRunning = value; }
+    }
+
+    private void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+        initialSpeed = speed;
+    }
+
+    private void Update()
+    {
+        OnInput();
+        OnRun();
+        OnRolling();
+    }
+
+    private void FixedUpdate()
+    {
+        OnMove();
+    }
+
+    #region Movement
+
+    void OnInput()
+    {
+        _direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+    }
+
+    void OnMove()
+    {
+        rig.MovePosition(rig.position + _direction * speed * Time.fixedDeltaTime);
+    }
+
+    void OnRun()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            speed = runSpeed;
+            _isRunning = true;
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            speed = initialSpeed;
+            _isRunning = false;
+        }
+    }
+
+    void OnRolling()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            _isRolling = true; // Ativa o rolling
+        }
+        if (Input.GetMouseButtonUp(1))
+        {
+            _isRolling = false; // Desativa quando solta o botão
+        }
+    }
+    #endregion
 }
---
+++ Chronicles of Emberlight/Assets/Scripts/JohnSnownAnimation.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JohnSnownAnimation : MonoBehaviour
+{
+    private JohnSnown johnSnown;
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        johnSnown = GetComponent<JohnSnown>();
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        OnMove();
+        OnRun();
+        OnRolling();
+    }
+    #region Movement
+    void OnMove()
+    {
+
+
+        if (johnSnown.direction.sqrMagnitude > 0)
+        {
+            anim.SetInteger("Transition", 1);
+        }
+        else
+        {
+            anim.SetInteger("Transition", 0);
+        }
+
+        if (johnSnown.direction.x > 0)
+        {
+            transform.eulerAngles = new Vector2(0, 0);
+        }
+
+        if (johnSnown.direction.x < 0)
+        {
+            transform.eulerAngles = new Vector2(0, 180);
+        }
+    }
+
+    void OnRun()
+    {
+        if (johnSnown.isRunning)
+        {
+            anim.SetInteger("Transition", 2);
+        }
+    }
+    void OnRolling()
+    {
+        if (johnSnown.isRolling)
+        {
+            anim.SetInteger("Transition", 3);
+        }
+    }
+    #endregion
+}
