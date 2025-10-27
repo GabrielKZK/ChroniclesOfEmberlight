644842f0eca84ed6e9541e1c36f6b5b010d35e16
GabrielKZK <gabriel.nasimeto34@gmail.com>
2025-10-27 02:53:51 -0300

    Adicionando novas sprites, NPCs e sistema de diálogo

---
+++ Chronicles of Emberlight/Assets/Scripts/Dialogue/DialogueControl.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueControl : MonoBehaviour
+{
+
+    [Header("Components")]
+    public GameObject dialogueObj;//janela do diálogo
+    public Image profileSprite;//sprite do perfil
+    public Text spechText;//texto da fala
+    public Text actorNameText;//Nome do NPC
+
+    [Header("Settings")]
+    public float typingSpeed;//velocidade da fala
+
+
+    //Variáveis de controle
+    private bool isShowing;//se a janela está visível
+    private int index;//Rodar os laços de repetição(Basicamente ele será utilizado para saber quantas falas estão sendo ditas até ser encerrado)
+    private string[] sentences;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+
+    IEnumerator TypeSentence()
+    {
+        foreach (char letter in sentences[index].ToCharArray())
+        {
+            spechText.text += letter;//Vai ser feito a alimentação de letra por letra no diálogo
+            yield return new WaitForSeconds(typingSpeed);
+        }
+    }
+
+    //Pular para a próxima  fase/fala
+    public void NextSentences()
+    {
+
+    }
+
+    //chamar a fala do npc
+    public void Speech(string[] txt)
+    {
+        if (!isShowing)
+        {
+            dialogueObj.SetActive(true);
+            sentences = txt;
+            StartCoroutine(TypeSentence());
+            isShowing = true;
+        }
+    }
+}
---
+++ Chronicles of Emberlight/Assets/Scripts/NPC/NPC_Dialogue.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC_Dialogue : MonoBehaviour
+{
+
+    public float dialogueRange;
+    public LayerMask playerLayer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        showDialogue();
+    }
+
+    void showDialogue()
+    {
+        Collider2D hit = Physics2D.OverlapCircle(transform.position, dialogueRange, playerLayer);
+
+        if (hit != null)
+        {
+            Debug.Log("Corno na área de colisão");
+        }
+        else
+        {
+
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(transform.position, dialogueRange);
+    }
+}
